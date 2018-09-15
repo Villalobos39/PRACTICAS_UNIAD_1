46abcbe3a7c6c54edfaf295e6e45d023184d3eca
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-09-15 14:48:45 -0700

    Add files via upload
    SUMA DE VECTORES

--- Program.cs
+++ Program.cs
@@ -4,32 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VECTORES
+namespace SUMA_VECTORES
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int suma = 0;
-            int[] MEMORIA = new int[15] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15};
+                int suma = 0;
+                int[] MEMORIA = new int[15]; // VECTOR DETERMINADO CON 15 ELEMENTOS
 
-           
+            Console.Write("________SUMA DE VECTORES _______\n");
+            for (int CONTADOR = 0; CONTADOR < 15; CONTADOR++) // CICLO FOR PARA CAPTUAR DATOS 
+                {
+                    Console.Write("INGRESA DATOS {0} : ", CONTADOR + 1);
+                    MEMORIA[CONTADOR] = int.Parse(Console.ReadLine());
+                }
 
-            for (int CONTAR = 0; CONTAR < 4; CONTAR++) 
-            {
-                suma = suma + MEMORIA[CONTAR];
+            for (int CONTAR = 0; CONTAR < 4; CONTAR++) // CICLO FOR PARA SUMAR LOS PRIMEROS 4 NUMEROS
+                {
+                    suma = suma + MEMORIA[CONTAR];
+                }
+                Console.WriteLine("LA SUMA DE LOS 4 NUMEROS ES : {0} ", suma);
+                Console.WriteLine("");
+                suma = 0;
+
+            for (int CONTAR = 0; CONTAR < 9; CONTAR++) // CICLO FOR PARA SUMAR LOS PRIMEROS 4 NUMEROS
+                 {
+                    suma = suma + MEMORIA[CONTAR];
+                 }
+                Console.WriteLine("LA SUMA DE LOS 9 NUMERO INGRESADOS ES : {0} ", suma); // RESULTADO
+
+                Console.ReadKey();
             }
-            Console.WriteLine("LA SUMA DE LOS 4 NUMEROS ES : {0} ", suma);
-            Console.WriteLine("");
-            suma = 0;
-
-            for (int CONTAR = 0; CONTAR < 9; CONTAR++)
-            {
-                suma = suma + MEMORIA[CONTAR];
-            }
-            Console.WriteLine("LA SUMA DE LOS 9 NUMERO INGRESADOS ES : {0} ", suma); 
-
-            Console.ReadKey();
         }
     }
-}
+    
+
